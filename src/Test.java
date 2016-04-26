190d020e150bf8cd4963fc00c7aa10a34fc9fa10
wbwriter <andreasfi@me.com>
2016-04-26 13:29:21 +0200

    Basic RW client

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -14,6 +15,8 @@ public class Test {
 		Socket clientSocket;
 		InetAddress serverAddress;
         String serverName = "192.168.108.10";
+
+		PrintWriter pout;
 	        
 			try {
 				serverAddress = InetAddress.getByName(serverName);
@@ -24,18 +27,28 @@ public class Test {
 				System.out.println("We got the connexion to  "+ serverAddress);
 				System.out.println("Will read data given by server:\n");
 				
-				
+
+				pout = new PrintWriter(mySocket.getOutputStream());
 
 				//create an input stream to read data from the server
 				BufferedReader buffin = new BufferedReader (new InputStreamReader (mySocket.getInputStream()));
 				
 				//Read a line from the input buffer coming from the server
-		        String message_distant = buffin.readLine();
-		        System.out.println(message_distant);
+				String message_distant="";
+				while(!message_distant.equals("quit")){
+					message_distant = buffin.readLine().trim();
+			        
+			        System.out.println(message_distant);
+			        
+			        pout.println(message_distant);
+					pout.flush();	
+				}
+		        
 				
 				//close the connection
 		        System.out.println("\nMessage read. Now dying...");
 		        mySocket.close();
+				pout.close();
 
 			}catch (UnknownHostException e) {
 				
@@ -47,8 +60,6 @@ public class Test {
 				
 				e.printStackTrace();
 			}
-	
-		
 
 	}
 
